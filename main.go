753b250ba7176ae8aa32405ea9be183c24bd3886
Bruno Andreo <brunolino2026@gmail.com>
2021-10-14 21:30:35 -0300

    Added requester

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"apphealthmonitor/files"
 	"fmt"
+	"net/http"
+	"time"
 )
 
 var FILE_NAME = "config.yaml"
@@ -10,5 +12,35 @@ var FILE_NAME = "config.yaml"
 func main() {
 	configs := files.LoadConfig(FILE_NAME)
 
-	fmt.Println(configs)
+	watch(configs)
+
+}
+
+func watch(configs files.Configs) {
+	for i := 1; i <= configs.Requests.Quantity; i++ {
+		fmt.Println("-----------------------")
+		fmt.Println("Start", i, "round")
+
+		for _, site := range configs.Sites {
+			statusCode, online := request(site)
+			fmt.Println(site, statusCode, online)
+		}
+
+		if i < configs.Requests.Quantity {
+			fmt.Println("Waiting next round")
+			time.Sleep(time.Duration(configs.Requests.DelaySeconds) * time.Second)
+		}
+	}
+}
+
+func request(site string) (int, bool) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	online := resp.StatusCode != http.StatusInternalServerError
+
+	return resp.StatusCode, online
 }
